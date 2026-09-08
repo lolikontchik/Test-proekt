e1bbefeeb1e6937b2d326cc3d7ded55c14f607c8
lolikontchik <maksimpuckov65098@gmail.com>
2026-09-08 11:42:02 +0700

    Решение задачки.

--- 2 kyrs/Source.cpp
+++ 2 kyrs/Source.cpp
@@ -1,1 +1,12 @@
-dshglasdj
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a, b;
+	cout << "a = " << endl; cin >> a;
+	cout << "b = " << endl; cin >> b;
+	cout << "res = " << a + b;
+	return 0;
+}
